fcc431c2115761f64c5f48907921f9778acaf987
LatexB <bartlomiej.latek3@gmail.com>
2022-05-31 00:02:31 +0200

    fix formatting

--- gui_impl.py
+++ gui_impl.py
@@ -1,14 +1,14 @@
+import matplotlib.pyplot as plt
 import streamlit as st
 from test import test_thd
 
 _, x, y = test_thd()
 
-import matplotlib.pyplot as plt
 
 fig, ax = plt.subplots()
-ax.plot(x,y)
+ax.plot(x, y)
 st.pyplot(fig)
 
 fig, ax = plt.subplots()
-ax.plot(x,y)
+ax.plot(x, y)
 st.pyplot(fig)
