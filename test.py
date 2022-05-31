f2b55bfe4d4aade4c7c258e4e0d041d989c5062b
Maciejeg <maciek.majek2@gmail.com>
2022-05-31 13:52:07 +0200

    Implement thd+n calculation

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-from utils import calculate_thd
-from scipy.fft import fft, fftfreq
+from utils import calculate_thd, calculate_thd_n, to_dB
 import numpy as np
 
 
@@ -19,6 +18,31 @@ def test_thd():
     assert (y >= 0).all(), "Negative FFT value"
 
 
+def test_thd_n():
+    f = 73.12
+    fs = 20 * f
+    N = 6000
+    T = 1.0 / (fs)
+    t = np.linspace(0, N * T, N, endpoint=False)
+    y = 0
+
+    for i in range(1, 10):
+        y += 1 / i**4 * np.sin(t * 2 * np.pi * f * i)
+
+    rng = np.random.default_rng(0)
+    noise_level = 0
+    noise = 10**(noise_level / 20) * rng.random(6000)
+    y += noise
+
+    thd = calculate_thd(y, f, fs)
+    thd_n, x, y, n = calculate_thd_n(y, f, fs, True)
+    assert thd == 0.06168333361611456, "THD calculation failed"
+    assert thd_n == 0.06292998187759988, "THD calculation failed"
+    assert (y >= 0).all(), "Negative FFT value"
+    assert thd_n >= thd, "THD+N must be bigger or equal to THD"
+
+
 if __name__ == "__main__":
     test_thd()
+    test_thd_n()
     print("Everything passed")
